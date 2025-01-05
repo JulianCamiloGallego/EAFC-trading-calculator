99ae164032530bf5217fdbbd87f699eda1241cb6
Julian Gallego Franco <jcgallego22@gmail.com>
2025-01-04 22:11:41 -0700

    Vite error

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/EAFC-trading-calculator/", // Set this to your repository name
+  base: "https://juliancamilogallego.github.io/EAFC-trading-calculator/", // Set this to your repository name
 });
